3a8d18d0b32f9c15f86d780544d6a873c112b611
Markus <marco.polo.1994@web.de>
2023-09-11 09:32:54 +0200

    remove debug

--- Editor/Helpers/Steam/SteamUploader.cs
+++ Editor/Helpers/Steam/SteamUploader.cs
@@ -160,27 +160,17 @@ namespace BuildManager{
         /// <param name="targets"></param>
         /// <param name="appID"></param>
         public void UploadHeadless(SuccessfulBuildTargets targets, int appID) {
-            foreach (var build in targets.builds) {
-                System.Console.WriteLine(build.buildTarget);
-            }
-
             System.Console.WriteLine("[Steam: Upload Default] " + ((targets != null && targets.builds != null) ? string.Join(",", targets.GetBuildTargets()) : "NULL!"));
             System.Console.WriteLine($"##### Start Upload: {DateTime.Now.ToString("HH:mm:ss")} #####");
             DateTime startTime = DateTime.Now;
 
             if (targets != null && targets.Count > 0) {
-
-                System.Console.WriteLine("targets != null && targets.Count > 0");
-
                 UpdateSelectedAppConfig(tempAppID: appID);
                 if (selectedAppConfig == null) {
                     Debug.Log("No application selected");
                     HeadlessBuild.WriteToProperties("Error", "30");
                     EditorApplication.Exit(30);
                 } else {
-
-                    System.Console.WriteLine("AppConfig: " + selectedAppConfig);
-
                     Settings.CacheDataPath();
                     Settings.CacheStreamingAssetsPath();
                     var buildMessage = CreateStandardBuildMessage(targets == null ? "DLCUpload" : string.Join(",", targets));
